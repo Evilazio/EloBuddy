96dbddb499c63a1859392d952e2a560bcc62010b
BuildTools <unconfigured@null.spigotmc.org>
2016-07-19 23:22:41 -0300

    Teemo addon 1.1.0.0

--- Teemonster/Program.cs
+++ Teemonster/Program.cs
@@ -12,6 +12,7 @@ using EloBuddy.SDK.Rendering;
 using EloBuddy.SDK.Menu;
 using EloBuddy.SDK.Menu.Values;
 
+
 namespace Teemonster
 {
     class Program
@@ -22,7 +23,7 @@ namespace Teemonster
         
         public static  AIHeroClient User = Player.Instance;
 
-        public static Menu Drawnigs, ComboMenu, KSMenu, HarassMenu, menu;
+        public static Menu Drawnigs, ComboMenu, KSMenu, HarassMenu, Auto, menu;
         private static Spell.Targeted Q;
         private static Spell.Active W;
         private static Spell.Skillshot R;
@@ -35,11 +36,14 @@ namespace Teemonster
             {
                 return;
             }
-            Bootstrap.Init(null);
+            
             Game.OnTick += OnTick;
             Drawing.OnDraw += Drawing_OnDraw;
+            Player.LevelSpell(SpellSlot.Q);
+            Player.DoEmote(Emote.Joke);
 
-            
+            Bootstrap.Init(null);
+
             Q = new Spell.Targeted(SpellSlot.Q, spellRange: 680);
 
             W = new Spell.Active(SpellSlot.W);
@@ -49,10 +53,25 @@ namespace Teemonster
             
 
             menu = MainMenu.AddMenu("Teemonster", "harigaren");
+
+            menu.AddLabel("TEEMONSTER");
+            menu.AddSeparator();
+            menu.AddLabel("Obrigado por usar o addon Teemonster. Boa sorte!");
+            menu.AddSeparator();
+            menu.AddLabel("Versao: 1.1.0.0   19/07/2016");
+            menu.AddSeparator();
+            menu.AddLabel("Por favor reportar qualquer bug no forum do elobuddy. E deixe sua sugestão.");
+            menu.AddSeparator();
+            menu.AddLabel("Addon criado por: Evilazio");
+
+
+
+
             ComboMenu = menu.AddSubMenu("Combo", "comboMenu");
             HarassMenu = menu.AddSubMenu("Harass", "harassMenu");
-            KSMenu = menu.AddSubMenu("KillSteal", "KSMenu");
+            KSMenu = menu.AddSubMenu("Kill Steal", "KSMenu");
             Drawnigs = menu.AddSubMenu("Drawnigs", "drawnigs");
+            Auto = menu.AddSubMenu("Auto Skill", "auto");
 
             ComboMenu.AddGroupLabel("CONFIGURAR COMBO:");
             ComboMenu.Add("qCombo", new CheckBox("Usar (Q) no combo"));
@@ -61,21 +80,18 @@ namespace Teemonster
             ComboMenu.Add("wManaCombo", new Slider("Mana minima necessaria para o W: {0}", 25, 0, 100));
             ComboMenu.AddSeparator();
             ComboMenu.Add("rCombo", new CheckBox("Usar (R) no combo"));
-            ComboMenu.Add("rQntCombo", new Slider("Manter pelomenos {0} cogumelo(s)", 0, 0, 3));
+            ComboMenu.Add("rComboQnt", new Slider("Manter pelomenos {0} cogumelo(s)", 0, 0, 3));
             ComboMenu.AddSeparator();
 
             HarassMenu.AddGroupLabel("CONFIGURAR HARASS:");
             HarassMenu.Add("qHarass", new CheckBox("Usar (Q) no Harass"));
             HarassMenu.AddSeparator();
             HarassMenu.Add("rHarass", new CheckBox("Usar (R) no Harass"));
-            HarassMenu.Add("rQntHarass", new Slider("Manter pelomenos {0} cogumelo(s)", 0, 0, 3));
+            HarassMenu.Add("rHarassQnt", new Slider("Manter pelomenos {0} cogumelo(s)", 0, 0, 3));
             HarassMenu.AddSeparator();
 
             KSMenu.AddGroupLabel("CONFIGURAR KS:");
             KSMenu.Add("qKS", new CheckBox("Usar (Q) no KS"));
-            KSMenu.AddSeparator();
-            KSMenu.Add("wKS", new CheckBox("Usar (W) no KS"));
-            KSMenu.Add("wManaKS", new Slider("Mana minima necessaria para o W: {0}", 40, 0, 100));
             KSMenu.AddSeparator();
 
             Drawnigs.AddGroupLabel("Drawnings ON/OFF:");
@@ -83,22 +99,47 @@ namespace Teemonster
             Drawnigs.AddSeparator();
             Drawnigs.Add("rDraw", new CheckBox("Draw (R)  AZUL / BLUE"));
 
-
+            Auto.AddGroupLabel("Auto Skill:");
+            Auto.Add("AutoONOFF", new CheckBox("AUTO SKILL ON/OFF"));
+            Auto.AddSeparator();
+            Auto.Add("invAuto", new CheckBox("AUTO SKILL INVISIVEL", false));
+            Auto.AddSeparator();
+            Auto.Add("qAuto", new CheckBox("Auto (Q) "));
+            Auto.Add("qAutoMana", new Slider("Auto (Q) se a mana for maior que: {0} %", 40, 0, 100));
+            Auto.AddSeparator();
+            Auto.Add("rAuto", new CheckBox("Auto (R) "));
+            Auto.Add("rAutoMana", new Slider("Auto (R) se a mana for maior que: {0} %", 40, 0, 100));
+            Auto.AddSeparator();
+            Auto.Add("rAutoQnt", new Slider("Auto (R) se o numero de cogumelos for maior que: {0} ", 1, 0, 3)); 
+            Auto.AddSeparator();
             
         }
 
         private static void Drawing_OnDraw(EventArgs args)
         {
-            new Circle { Color = Color.FromArgb(60, 200, 200, 0), Radius = Q.Range, BorderWidth = 2f }.Draw(User.Position);
-            new Circle { Color = Color.FromArgb(60, 0, 50, 200), Radius = R.Range, BorderWidth = 5f }.Draw(User.Position);
-            new Circle { Color = Color.FromArgb(30,200,200,200), Radius = 1100, BorderWidth = 5f }.Draw(User.Position);
+            if (Drawnigs["qDraw"].Cast<CheckBox>().CurrentValue)
+            {
+                new Circle { Color = Color.FromArgb(60, 200, 200, 0), Radius = Q.Range, BorderWidth = 5f }.Draw(User.Position);
+            }
+            if (Drawnigs["rDraw"].Cast<CheckBox>().CurrentValue)
+            {
+                new Circle { Color = Color.FromArgb(60, 0, 50, 200), Radius = R.Range, BorderWidth = 2f }.Draw(User.Position);
+            }
+            
+                new Circle { Color = Color.FromArgb(30,200,200,200), Radius = 1100, BorderWidth = 1f }.Draw(User.Position);
+
+            
         }
+
+        
 
         public static void OnTick(EventArgs args)
         {
+            var target = TargetSelector.GetTarget(1100, damageType: DamageType.Magical);
+
             Rquantidade();
-            KillSteal();
-            
+            KillSteal(target);
+            AutoSkill(target);
 
             if (R.Level == 0)
             {
@@ -118,39 +159,50 @@ namespace Teemonster
             }
 
 
+            //foreach (var buff in Player.Instance.Buffs)
+            //{
+            //    Chat.Say("BuffName: {0}, Stacks: {1}", buff.Name, buff.Count);
+            //}
+            
+            
 
             if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
             {
-                Combo();
+                Combo(target);
             }
             if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass))
             {
-                Harass();
+                Harass(target);
             }
+
+
+            
 
         }
 
-        private static void Combo()
+        private static void Combo(Obj_AI_Base target)
         {
-            
 
-            var target = TargetSelector.GetTarget(1100, damageType: DamageType.Magical);
 
-            if(target == null)
+            if (target == null)
             {
                 return;
             }
+
+         
 
 
             if (ComboMenu["qCombo"].Cast<CheckBox>().CurrentValue && 
                 target.IsValidTarget(Q.Range) && Q.IsReady())
             {
                 Q.Cast(target);
+                
             }
+            
 
 
             if (ComboMenu["wCombo"].Cast<CheckBox>().CurrentValue && User.ManaPercent > ComboMenu["wManaCombo"].Cast<Slider>().CurrentValue &&
-                User.Distance(target) > User.AttackRange && User.Distance(target) < 1100)
+                User.Distance(target) >= User.AttackRange && User.Distance(target) < 1100)
             {
                 W.Cast();
             }
@@ -158,7 +210,7 @@ namespace Teemonster
             
             
             var Rpred = R.GetPrediction(target);
-            if (ComboMenu["rCombo"].Cast<CheckBox>().CurrentValue && Rquantidade() > ComboMenu["rCombo"].Cast<Slider>().CurrentValue &&
+            if (ComboMenu["rCombo"].Cast<CheckBox>().CurrentValue && Rquantidade() > ComboMenu["rComboQnt"].Cast<Slider>().CurrentValue &&
                 target.IsValidTarget(R.Range) && R.IsReady() && Rpred.HitChance >= HitChance.High)
             {
                 R.Cast(target.Position);
@@ -166,28 +218,68 @@ namespace Teemonster
             }
         }
 
-        private static void Harass()
+        private static void Harass(Obj_AI_Base target)
         {
-            var target = TargetSelector.GetTarget(1100, damageType: DamageType.Magical);
+            
+            
+            if (target == null)
+            {
+                return;
+            }
 
-            if (ComboMenu["qHarass"].Cast<CheckBox>().CurrentValue &&
+            
+            if (HarassMenu["qHarass"].Cast<CheckBox>().CurrentValue &&
+               target.IsValidTarget(Q.Range) && Q.IsReady())
+            {
+                Q.Cast(target);
+            }
+            
+
+            var Rpred = R.GetPrediction(target);
+            if (HarassMenu["rHarass"].Cast<CheckBox>().CurrentValue && Rquantidade() > ComboMenu["rHarassQnt"].Cast<Slider>().CurrentValue &&
+               target.IsValidTarget(R.Range) && R.IsReady() && Rpred.HitChance >= HitChance.High )
+            {
+                
+                R.Cast(target.Position);
+            }
+            
+
+
+        }
+
+        private static void AutoSkill(Obj_AI_Base target)
+        {
+            if ( !Auto["invAuto"].Cast<CheckBox>().CurrentValue && User.HasBuff("camouflagestealth") )
+            {
+                
+                return;
+            }
+            
+
+            if (target == null)
+            {
+                return;
+            }
+
+
+            
+
+            if (Auto["qAuto"].Cast<CheckBox>().CurrentValue && User.ManaPercent > Auto["qAutoMana"].Cast<Slider>().CurrentValue  &&
                target.IsValidTarget(Q.Range) && Q.IsReady())
             {
                 Q.Cast(target);
             }
 
-            if (ComboMenu["rHarass"].Cast<CheckBox>().CurrentValue && Rquantidade() > ComboMenu["rQntHarass"].Cast<Slider>().CurrentValue &&
-               target.IsValidTarget(R.Range) && R.IsReady())
+            if (Auto["rAuto"].Cast<CheckBox>().CurrentValue && Rquantidade() > Auto["rAutoQnt"].Cast<Slider>().CurrentValue && User.ManaPercent > Auto["rAutoMana"].Cast<Slider>().CurrentValue &&
+               target.IsValidTarget(R.Range) && R.GetPrediction(target).HitChance >= HitChance.Medium)
             {
                 R.Cast(target.Position);
             }
-
-
         }
 
-        public static void KillSteal()
+        public static void KillSteal(Obj_AI_Base target)
         {
-            var target = TargetSelector.GetTarget(Q.Range, DamageType.Magical);
+            
             if (target != null && GetQDamage(target) >= target.Health)
             {
                 Q.Cast(target);
